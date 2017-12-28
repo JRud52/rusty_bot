2f22f9b71aa7c15c7b98b7797c36c52491fa4775
Justin Rhude <justin.rhude52@gmail.com>
2017-12-28 18:14:18 -0500

    added example bot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#[macro_use] extern crate serenity;
+
+use serenity::client::Client;
+use serenity::prelude::EventHandler;
+use serenity::framework::standard::StandardFramework;
+use std::env;
+
+struct Handler;
+
+impl EventHandler for Handler {}
+
 fn main() {
-    println!("Hello, world!");
+    // Login with a bot token from the environment
+    let mut client = Client::new(&env::var("RUSTY_TOKEN").expect("token"), Handler);
+    client.with_framework(StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .on("ping", ping));
+
+    // start listening for events by starting a single shard
+    let _ = client.start();
 }
+
+command!(ping(_context, message) {
+    let _ = message.reply("Pong!");
+});
